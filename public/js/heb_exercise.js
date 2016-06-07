40db971f2f12aa29dffa4df283d6cb1a70f6208f
Craig Rupp <craigrupp@utexas.edu>
2016-06-07 16:48:17 -0500

    6/7 Afternoon Exercises

---
+++ public/js/heb_exercise.js
@@ -0,0 +1,80 @@
+'use strict';
+
+// console.log('heb question');
+// var cameronCost = 180;
+// var ryanCost = 250;
+// var georgeCost = 320;
+
+// var discountCostRequirement = 200;
+// var discount = 0.35;
+
+// var cameronCostWithDiscount;
+// if (cameronCost > discountCostRequirement) {
+//     cameronCostWithDiscount = cameronCost - (cameronCost * discount);
+// } else {
+//     cameronCostWithDiscount = cameronCost;
+// }
+// console.log('Cameron: ' + cameronCost + ', ' + cameronCostWithDiscount);
+
+// var ryanCostWithDiscount;
+// if (ryanCost > discountCostRequirement) {
+//     ryanCostWithDiscount = ryanCost - (ryanCost * discount);
+// } else {
+//     ryanCostWithDiscount = ryanCost;
+// }
+// console.log('Ryan: ' + ryanCost + ', ' + ryanCostWithDiscount);
+
+// var georgeCostWithDiscount;
+// if (georgeCost > discountCostRequirement) {
+//     georgeCostWithDiscount = georgeCost - (georgeCost * discount);
+// } else {
+//     georgeCostWithDiscount = georgeCost;
+// }
+// console.log('Ryan: ' + georgeCost + ', ' + georgeCostWithDiscount);
+
+var customerCost = 0;
+var individualCustomer = [];
+var discountCostRequirement = 200;
+var discount = 0.65;
+var moreProducts;
+var costOfProduct;
+
+do {  
+var customer = {
+	name: null,
+	products: []
+}
+
+customer.name = prompt('What is your name?');
+individualCustomer.push(customer);
+
+function productPricesEntered (){
+	costOfProduct = prompt('What was the cost of your item?');
+	customer.products.push(Number(costOfProduct));
+	
+} 
+do {
+	productPricesEntered();
+	moreProducts = confirm('Would you like to add any other items?');
+} while (moreProducts == true);
+
+function confirmDiscount (){
+	var totalCost = 0;
+	customer.products.forEach(function (element) {
+		totalCost = totalCost + element;
+	});
+	if (totalCost > discountCostRequirement){
+		totalCost = totalCost * discount;
+		alert(customer.name + ', your total price to pay is ' +  totalCost + '. Congrats on the discount!');	
+	} else {
+		alert(customer.name + ', your total price to pay is ' + totalCost + '. Sorry brah.');
+	}
+};
+
+confirmDiscount();
+var moreCustomers = confirm('Is there another customer?')
+} while (moreCustomers)
+
+
+
+
--- public/js/objects.js
+++ public/js/objects.js
@@ -1,24 +1,24 @@
 (function(){
-    "use strict";
+	"use strict";
 
-    // TODO: Create person object
-    // var person = todo;
-        // TODO: Create firstName and lastName properties in your person object; assign your name to them
-
-
-    var person = {};
-    person.firstName = 'Jorge';
-    person.lastName = 'ConFuego';
+	// TODO: Create person object
+	// var person = todo;
+		// TODO: Create firstName and lastName properties in your person object; assign your name to them
 
 
-    // TODO: Add a sayHello method to the person object that
-    //       alerts a greeting using the firstName and lastName properties
+	var person = {}
+		person.firstName = 'Jorge';
+		person.lastName = 'ConFuego';
 
-    person.sayHello = function(){
-    	alert('Hello ' + this.firstName + ' ' + this.lastName + ' welcome to the Thunder Dome!');
-    }
 
-    // Say hello from the person object.
-    // person.sayHello();
-    person.sayHello();
+		// TODO: Add a sayHello method to the person object that
+		//       alerts a greeting using the firstName and lastName properties
+
+		person.sayHello = function(){
+			alert('Hello ' + this.firstName + ' ' + this.lastName + ' welcome to the Thunder Dome!');
+		}
+	 
+	// Say hello from the person object.
+	// person.sayHello();
+	person.sayHello();
 })();
