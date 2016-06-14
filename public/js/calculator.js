7d8a82fed79838555f0b16830bf6391925fa7718
Craig Rupp <craigrupp@utexas.edu>
2016-06-14 14:44:35 -0500

    Add More Ofen,Craig

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -5,6 +5,9 @@ var thirdBox = document.getElementById("right-output");
 var firstBox = document.getElementById("left-output");
 var equalButton = document.getElementById("equal_button");
 var clearButton = document.getElementById("clear_button");
+var deleteButton = document.getElementById("back_button");
+var squareRootButton = document.getElementById("square_root")
+var raiseToButton = document.getElementById("power_raised");
 
 function storefirstItem(){
  if (operatorBox.value == '') {
@@ -26,24 +29,44 @@ function clear(){
 }
 function doMath(){
 	if(operatorBox.value == "+"){
-		firstBox.value = (parseInt(firstBox.value)) + (parseInt(thirdBox.value));
+		firstBox.value = (parseFloat(firstBox.value)) + (parseFloat(thirdBox.value));
 		operatorBox.value = ''
 		thirdBox.value = ''
 	} else if (operatorBox.value == "-") {
-		firstBox.value = (parseInt(firstBox.value)) - (parseInt(thirdBox.value));
+		firstBox.value = (parseFloat(firstBox.value)) - (parseFloat(thirdBox.value));
 		operatorBox.value = ''
 		thirdBox.value = ''
 	} else if (operatorBox.value == '*'){
-		firstBox.value = (parseInt(firstBox.value)) * (parseInt(thirdBox.value));
+		firstBox.value = (parseFloat(firstBox.value)) * (parseFloat(thirdBox.value));
 		operatorBox.value = ''
 		thirdBox.value = ''
 	} else if (operatorBox.value == '/'){
-		firstBox.value = (parseInt(firstBox.value)) / (parseInt(thirdBox.value));
+		firstBox.value = (parseFloat(firstBox.value)) / (parseFloat(thirdBox.value));
+		operatorBox.value = ''
+		thirdBox.value = ''
+	}
+	else if (operatorBox.value == '^'){
+		firstBox.value = Math.pow((parseFloat(firstBox.value)), (parseFloat(thirdBox.value)));
 		operatorBox.value = ''
 		thirdBox.value = ''
 	}
 }
-
+function remove(){
+	if(operatorBox.value == ''){
+		firstBox.value = firstBox.value.substring(0, firstBox.value.length - 1);
+	}
+	else if(operatorBox.value !== ''){
+		thirdBox.value = thirdBox.value.substring(0, thirdBox.value.length - 1);
+	}
+}
+function sqRoot(){
+	if(operatorBox.value == '')
+	firstBox.value = Math.sqrt(firstBox.value);
+}	
+// function raisedTo(){
+// 	if(operatorBox.value == '^')
+// 	firstBox.value = Math.pow(firstBox.value, firstBox.value);
+// }
 var numbers = document.getElementsByClassName("number")
 for(var i = 0; i < numbers.length; i++) {
 	numbers[i].addEventListener('click', storefirstItem);
@@ -57,6 +80,11 @@ for(var i = 0; i < operators.length; i++){
 
 clearButton.addEventListener('click', clear);
 equalButton.addEventListener('click', doMath);
+deleteButton.addEventListener('click', remove);
+squareRootButton.addEventListener('click', sqRoot);
+// raiseToButton.addEventListener('click', raisedTo);
+
+
 
 
 
