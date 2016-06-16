18d48cd3b1791ce20c5c667a7ef910be34218d3c
Craig Rupp <craigrupp@utexas.edu>
2016-06-16 13:38:19 -0500

    6/16 early afternoon add

--- public/js/grades-events.js
+++ public/js/grades-events.js
@@ -1,17 +1,40 @@
 'use strict';
+var student = {
+    awesomeGrade: 80,
+    name: null,
+    subjects: [],
+    calculateAverage: function () {
+        var average = 0;
+        this.subjects.forEach(function (subject) {
+            average += subject.grade;
+        });
+        return average / this.subjects.length;
+    },
+    addSubject: function (name, grade) {
+        var subject = {
+            name: name,
+            grade: grade
+        };
+        this.subjects.push(subject);
+    },
+    isAwesome: function () {
+        return this.calculateAverage() > this.awesomeGrade;
+    }
+}
 // Button definition Detailing
-var saveButton = document.getElementById("save-name");
-var gradeButton = document.getElementById("add-grade");
-var calcButton = document.getElementById("calculate-average");
+var saveButton = $("#save-name");
+var gradeButton = $("#add-grade");
+var calcButton = $("#calculate-average");
 // Define Variable for Student Name
-var nameOfStudent = document.getElementById("student-name");
+var nameOfStudent = $("#student-name");
+// var nameOfStudent = $("#student-name");
 
 function recordName(){
-    var nameEntered = document.getElementById("name")
-    student.name = nameEntered.value;
-    gradeButton.removeAttribute('disabled');
-    calcButton.removeAttribute('disabled');
-    nameOfStudent.innerText = student.name;
+    var nameEntered = $("#name")
+    student.name = nameEntered.val();
+    gradeButton.removeAttr('disabled');
+    calcButton.removeAttr('disabled');
+    nameOfStudent.text() = student.name;
 
 }
 // Add variables in function for the innerText to apply
@@ -19,15 +42,34 @@ function recordName(){
 // var subjectEntered = document.getElementById("subject");
 
 function inputGrades(){
-    var gradeEntered = document.getElementById("grade");
-    var subjectEntered = document.getElementById("subject");
-    student.addSubject(subjectEntered.value, parseInt(gradeEntered.value));
+    var gradeEntered = $("#grade");
+    var subjectEntered = $("#subject");
+    student.addSubject(subjectEntered.val(), parseInt(gradeEntered.val()));
 
-    var tableEntries = document.getElementById("grades");
-    tableEntries.innerHTML = '<tr><td>' + subjectEntered.value + '</td><td>' +
-    gradeEntered.value + '</td></tr>' + tableEntries.innerHTML;
+    var tableEntries = $("#grades");
+    tableEntries.html( '<tr><td>' + subjectEntered.val() + '</td><td>' +
+    gradeEntered.val() + '</td></tr>' + tableEntries.html());
 }
-console.log(inputGrades);
+// console.log(inputGrades);
 
-saveButton.addEventListener('click', recordName);
-gradeButton.addEventListener('click', inputGrades);
+function areYouAwesome() {
+    inputGrades();
+    var average = student.calculateAverage();
+    $('#student-average').html(average);
+
+    if (student.isAwesome()) {
+        $('#student-awesome').removeAttr('class');
+        $('#student-practice').attr('class', 'hidden');
+    } else {
+        $('#student-practice').removeAttr('class');
+        $('#student-awesome').attr('class', 'hidden');
+    }
+
+    gradeButton.attr('disabled', true);
+    calcButton.attr('disabled', true);
+}
+
+
+saveButton.on('click', recordName);
+gradeButton.on('click', inputGrades);
+calcButton.on('click', areYouAwesome);
---
+++ public/js/play_around.js
@@ -0,0 +1,51 @@
+"use strict";
+
+console.log('Hello');
+
+var myNameIs = 'Craig';
+
+function sayHello(name){
+	console.log("Hello " + name);
+}
+sayHello(myNameIs);
+console.log(sayHello);
+
+
+function sleepHours(hours){
+	if(hours >= 8) {
+		return "Getting plenty of sleep";
+	} else{
+		console.log("Get more sleep");
+	}
+}
+
+sleepHours(7);
+
+var x = myFunction(4, 3);        
+
+function myFunction(a, b) {
+    return a * b;               
+}
+console.log(x);
+
+for (var counter = 1; counter < 11; counter++) {
+	console.log(counter);
+}
+
+var names = [ 'Ernesto', 'TJ', 'Alan', 'Robert'];
+
+	// for (var i = 0; i < names.length; i++) {
+	// 	console.log('The name at index '+ i + ' is: ' + names[i]);
+	// }
+
+	names.forEach(function(element, index, array){
+		console.table('The name at index ' + index + ' is: ' + element);
+	});
+ 
+for (var i = 10; i < 0; i--) {
+	console.log(i);
+}
+
+for(var count = 10; i <=0; i--){
+	console.log(count);
+}
