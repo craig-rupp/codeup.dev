c2bcdc04334dc35deacd048d2860fcec33d71f19
Craig Rupp <craigrupp@utexas.edu>
2016-08-03 09:34:04 -0500

    Parks loaded

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,29 +1,29 @@
 <?php 
 
-// const DB_HOST = '127.0.0.1';
-// const DB_NAME = 'parks_db';
-// const DB_USER = 'parks_user';
-// const DB_PASS = 'rocks';
-
-// require_once '../db_connect.php';
-function getDemParks(){
+require_once '../db_connect.php';
+require_once '../input.php';
+function getDemParks($dbc){
 	
-	require_once '../park_seeder.php';
-	require_once '../input.php';
+	//require_once '../park_seeder.php';
+	
 
 
-	$page = Input::get($page);
-	// Input::get('page', 1) < 0 ? 1 : Input::get('page');
-	$limit = 4;
-    $offset = ($page - 1) * $limit;
+	// $page = Input::get($page);
+	// // Input::get('page', 1) < 0 ? 1 : Input::get('page');
+	// $limit = 4;
+ //    $offset = ($page - 1) * $limit;
 
-	$stmt = $dbc->query('SELECT * FROM national_parks LIMIT 4');
+	$stmt = $dbc->query('SELECT * FROM national_parks');
 
 	$parks = [];
-	while($parks = $stmt->fetch(PDO::FETCH_ASSOC)) {
+	while($park = $stmt->fetch(PDO::FETCH_ASSOC)) {
 		$parks[] = $park;
 	}
+	return [
+	 'parks' => $parks
+	];
 }
+extract(getDemParks($dbc));
 
 ?>
 
@@ -60,17 +60,42 @@ function getDemParks(){
 							</tr>
 						<?php endforeach; ?>
 					</tbody>
+		              <tfoot>
+		                <tr>
+		                    <td colspan="4">
+		                        <nav aria-label="Page navigation" class="text-center">
+		                            <ul class="pagination">
+		                                <li>
+		                                    <a href="?page=<? $offset - 1?>" aria-label="Previous">
+		                                        <span aria-hidden="true">&laquo;</span>
+		                                    </a>
+		                                </li>
+		                                <li><a href="?page=1">1</a></li>
+		                                <li><a href="?page=2">2</a></li>
+		                                <li><a href="?page=3">3</a></li>
+		                                <li><a href="?page=4">4</a></li>
+		                                <li><a href="?page=5">5</a></li>
+		                                <li>
+		                                    <a href="?page=<? $page +1 ?>" aria-label="Next">
+		                                        <span aria-hidden="true">&raquo;</span>
+		                                    </a>
+		                                </li>
+		                            </ul>
+		                        </nav>
+		                    </td>
+		                </tr>
+		              </tfoot>
 				</table>
 			</section>
 		</div>
-		<script
-		    src="https://code.jquery.com/jquery-2.2.4.min.js"
-		    integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
-		    crossorigin="anonymous"></script>
+	<script
+	    src="https://code.jquery.com/jquery-2.2.4.min.js"
+	    integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+	    crossorigin="anonymous"></script>
 
-		<script
-		    src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
-		    integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
-		    crossorigin="anonymous"></script>
+	<script
+	    src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+	    integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+	    crossorigin="anonymous"></script>
 	</body>
 </html>
