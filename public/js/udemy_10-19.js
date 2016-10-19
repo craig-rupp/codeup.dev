89535ae1178c22388dc216cb6818c650fbbc87b8
Craig Rupp <craigrupp@utexas.edu>
2016-10-19 13:08:02 -0500

    Changing CSS on clicks

--- public/js/udemy_10-19.js
+++ public/js/udemy_10-19.js
@@ -3,17 +3,19 @@
 alert("Udemy Linking External JS");
 
 $('#circle').click(function(){
-	alert($("p").html("New text"));
+	$(".square").fadeOut();
 });
 // $('.square').click(function(){
-// 	alert("Square clicked");
+// 	alert($('.square').css("width"));
 // })
 // $("div").click(function(){
-// 	alert("You have clicked a div");
+// 	//$(this).hide();
+// 	//$(this).css("display", "none");
+// 	$(this).fadeOut();
 // })
 // $('.square').hover(function(){
 // 	alert("You've hovered over a square");
 // })
-$('.square').click(function(){
-	$("iframe").attr("src", "http://www.arsenal.com");
-});
+// $('.square').click(function(){
+// 	$("iframe").attr("src", "http://www.arsenal.com");
+// });
