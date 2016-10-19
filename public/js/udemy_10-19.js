4fc7e57f4f17efa8212fcadfeb67d1433e4c0790
Craig Rupp <craigrupp@utexas.edu>
2016-10-19 11:21:06 -0500

    Time to start rolling, set-up there

--- public/js/udemy_10-19.js
+++ public/js/udemy_10-19.js
@@ -1,3 +1,17 @@
 'use strict';
 
 alert("Udemy Linking External JS");
+
+$('h1').click(function(){
+			$(this).css('background-color', 'green');
+		});
+		$('p').dblclick(function(){
+			$('p').css('font-size', '18px');
+		});
+		$('li').hover(function(){
+					$(this).css('color', 'red');
+				},
+				function(){
+					$(this).css('color', 'black');
+				}
+			);
