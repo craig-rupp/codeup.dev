8e0e16f93c73bc707dc08f1427111490c6272a9a
Craig Rupp <craigrupp@utexas.edu>
2016-10-19 12:54:04 -0500

    Change attribute jquery

--- public/js/udemy_10-19.js
+++ public/js/udemy_10-19.js
@@ -3,11 +3,17 @@
 alert("Udemy Linking External JS");
 
 $('#circle').click(function(){
-	alert("Circle Clicked");
+	alert($("p").html("New text"));
 });
-$('.square').click(function(){
-	alert("Square clicked");
-})
+// $('.square').click(function(){
+// 	alert("Square clicked");
+// })
 // $("div").click(function(){
 // 	alert("You have clicked a div");
 // })
+// $('.square').hover(function(){
+// 	alert("You've hovered over a square");
+// })
+$('.square').click(function(){
+	$("iframe").attr("src", "http://www.arsenal.com");
+});
