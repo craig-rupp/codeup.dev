dc5e0ab0cebc2d597c73b7660dfff4c3345905d6
Craig Rupp <craigrupp@utexas.edu>
2016-10-19 16:23:19 -0500

    Animate

--- public/js/udemy_10-19.js
+++ public/js/udemy_10-19.js
@@ -2,9 +2,9 @@
 
 alert("Udemy Linking External JS");
 
-$('#circle').click(function(){
-	$(".square").fadeOut();
-});
+// $('#circle').click(function(){
+// 	$(".square").fadeOut();
+// });
 // $('.square').click(function(){
 // 	alert($('.square').css("width"));
 // })
@@ -20,11 +20,21 @@ $('#circle').click(function(){
 // 	$("iframe").attr("src", "http://www.arsenal.com");
 // });
 
-$('#fadder').click(function(){
-	$('p').fadeOut(1500, function(){
-		alert("FadeOut Complete");
-	});
-});
-$('#fadeIn').click(function(){
-	$('p').fadeIn(1500);
+// $('#fadder').click(function(){
+// 	$('p').fadeOut(1500, function(){
+// 		alert("FadeOut Complete");
+// 	});
+// });
+// $('#fadeIn').click(function(){
+// 	$('p').fadeIn(1500);
+// })
+
+$('#circle').click(function(){
+	$(this).animate({
+		width:"300px",
+		height:"300px",
+		borderRadius:"150px",
+		marginLeft:"100px",
+		marginTop:"100px"
+		}, 800);
 })
