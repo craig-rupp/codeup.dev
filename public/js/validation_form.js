d4016dd5cd1002f2dbdeadd0a1755e973bfd5790
Craig Rupp <craigrupp@utexas.edu>
2016-10-19 19:12:44 -0500

    Validation process through jquery, not as kind as laravel

---
+++ public/js/validation_form.js
@@ -0,0 +1,36 @@
+'use strict';
+
+console.log("Hey were linked correctly");
+
+$('#validation').submit(function(event){
+	var errorMessage = '';
+	event.preventDefault();
+	function isValidEmailAddress(emailAddress) {
+    var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+    return pattern.test(emailAddress);
+	};
+
+	if(!isValidEmailAddress($("#email").val())){
+		errorMessage = "Please enter a valid email address";
+	}
+
+	if(!$.isNumeric($('#phone').val())){
+		errorMessage = errorMessage + "<br />Please enter a valid phone number";
+	}
+
+	if($('#pass').val() != $('#confirm_pass').val()){
+		errorMessage = errorMessage + "<br />Your Passwords do not match";
+	} 
+
+	if(errorMessage == '') {
+
+		alert("Success");
+
+	} else {
+		
+		$('#error').html(errorMessage);
+	}
+
+
+	
+});
