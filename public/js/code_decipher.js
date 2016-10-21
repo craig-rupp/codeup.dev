82e8abccea3afebaf47791a578980c4c52d02ad3
Craig Rupp <craigrupp@utexas.edu>
2016-10-20 22:03:21 -0500

    Divs and toggle working

--- public/js/code_decipher.js
+++ public/js/code_decipher.js
@@ -8,7 +8,21 @@ var containerHeight = windowHeight - menuBarHeight;
 
 $(".codeContainer").height(containerHeight + "px");
 
+$(".toggle").click(function(){
+	$(this).toggleClass("selected");
+	var activeDiv = $(this).html();
+	//alert(activeDiv);
+	$("#" + activeDiv + "Container").toggle();
 
+	var showingDivs = $(".codeContainer").filter(function(){
+		return($(this).css("display") != "none"); //maybe discuss a bit tomorrow
+	}).length;
+	//alert(showingDivs);
+
+	var width = 100/showingDivs;
+
+	$(".codeContainer").width(width + "%");
+});
 
 
 
